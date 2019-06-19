300f0768c529833d5bc0afc2d39ab7de0397f881
lizw <lzw1000000@163.com>
2019-06-19 12:48:12 +0800

    ...

--- clever-template-server/src/main/java/org/clever/template/controller/QueryPageController.java
+++ clever-template-server/src/main/java/org/clever/template/controller/QueryPageController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 作者： lzw<br/>
  * 创建时间：2019-06-13 16:50 <br/>
@@ -32,5 +35,21 @@ public class QueryPageController {
     @GetMapping("/find2")
     public IPage<Permission> findPermission(QueryPermissionReq query) {
         return queryPageService.findPermission(query);
+    }
+
+    @GetMapping("detail_data")
+    public Object getDetailData() throws InterruptedException {
+        Thread.sleep(1000 * (int) (Math.random() * 5));
+        Map<String, Object> data = new HashMap<>();
+        data.put("name", "freemenL");
+        data.put("dutyType", "早班");
+        data.put("deptName", "技术研发部 xxx部门 xxx部门 xxx部门");
+        data.put("startTime", "9:00");
+        data.put("endTime", "6:00");
+        data.put("onRange", "9:00-9:30");
+        data.put("offRange", "6:00-11:59");
+        data.put("duration", "1小时");
+        data.put("stats", 1);
+        return data;
     }
 }
